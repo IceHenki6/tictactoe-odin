9259a3a38dd5218b0805f4127316aa2078d9db11
Federico <fritzmaximovicz@gmail.com>
2022-12-12 16:26:17 -0300

    Fixed bug that allowed player to trigger a new turn after clicking an already occupied cell

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,6 @@ import GameResult from "./GameResult.js";
 import Player from "./Player.js";
 
 
-const game = document.querySelector('.game');
 const gameGrid = document.querySelector('#grid-container');
 
 
@@ -13,41 +12,39 @@ const player1 = Player.createPlayer('Player 1', 'X');
 const player2 = Player.createPlayer('Player 2', 'Y');
 
 
-console.log(GameBoard);
 let playerOnTurn = player1;
 GameBoardView.showPlayer(playerOnTurn);
 
 gameGrid.addEventListener('click', (event)=>{
-    GameBoardView.interaction(event, playerOnTurn);
-    const rowsChecked = GameBoard.checkRows();
-    const columnsChecked = GameBoard.checkColumns();
-    const diagonalsChecked = GameBoard.checkDiagonals();
-
+    if(event.target.textContent == ''){
+        GameBoardView.interaction(event, playerOnTurn);
+        const rowsChecked = GameBoard.checkRows();
+        const columnsChecked = GameBoard.checkColumns();
+        const diagonalsChecked = GameBoard.checkDiagonals();
     
-
-    if(rowsChecked.win){
-        console.log(`Win on row player ${rowsChecked.mark}`);
-        GameResult.displayWinner(playerOnTurn.name);
-        GameBoardView.highlightRow(rowsChecked.winningRow);
         
-    }else if(columnsChecked.win){
-        console.log(`Win on column player ${columnsChecked.mark}`);
-        GameResult.displayWinner(playerOnTurn.name);
-        GameBoardView.highlightColumn(columnsChecked.winningColumn);
-    }else if(diagonalsChecked.win){
-        console.log(`Win on diagonal player ${diagonalsChecked.mark}`);
-        GameResult.displayWinner(playerOnTurn.name);
-        GameBoardView.highlightDiagonal(diagonalsChecked.winningDiagonal);
-    }else if (GameBoard.areCellsComplete()){
-        GameResult.displayTie();
+    
+        if(rowsChecked.win){
+            GameResult.displayWinner(playerOnTurn.name);
+            GameBoardView.highlightRow(rowsChecked.winningRow);
+            
+        }else if(columnsChecked.win){
+            GameResult.displayWinner(playerOnTurn.name);
+            GameBoardView.highlightColumn(columnsChecked.winningColumn);
+        }else if(diagonalsChecked.win){
+            GameResult.displayWinner(playerOnTurn.name);
+            GameBoardView.highlightDiagonal(diagonalsChecked.winningDiagonal);
+        }else if (GameBoard.areCellsComplete()){
+            GameResult.displayTie();
+        }
+    
+        if(playerOnTurn==player1){
+            playerOnTurn = player2;
+        }else{
+            playerOnTurn = player1;
+        }
+        GameBoardView.showPlayer(playerOnTurn);
     }
-
-    if(playerOnTurn==player1){
-        playerOnTurn = player2;
-    }else{
-        playerOnTurn = player1;
-    }
-    GameBoardView.showPlayer(playerOnTurn);
 })
 
 
